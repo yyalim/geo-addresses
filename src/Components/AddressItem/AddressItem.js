8355944a3574726a4744ee38e4859f227141d755
yyalim <yusufyalim@gmail.com>
2020-12-07 16:05:43 +0100

    Make AddressItem pure component

--- src/Components/AddressItem/AddressItem.js
+++ src/Components/AddressItem/AddressItem.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { memo } from 'react';
 import './AddressItem.css';
 
-export default function AddressItem({ id, name, address }) {
+function areEqual(prevProps, nextProps) {
+  return prevProps.id === nextProps.id &&
+    prevProps.name === nextProps.name &&
+    prevProps.address === nextProps.address
+}
+
+const AddressItem = memo(function({ id, name, address }) {
   return (
     <div className="address-item">
       <h2 className="address-item__heading">{ name }</h2>
       <p className="address-item__content">{ address }</p>
     </div>
   );
-}
+}, areEqual);
+
+export default AddressItem;
